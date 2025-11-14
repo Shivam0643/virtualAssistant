e329ec443d86f50aeebf04a2c0ed187bd551d6e2
Shivam Lashkari <159267600+Shivam0643@users.noreply.github.com>
2025-11-14 13:41:31 +0530

    Update Customize.jsx

--- frontend/src/pages/Customize.jsx
+++ frontend/src/pages/Customize.jsx
@@ -8,7 +8,7 @@ import image4 from "../assets/image4.png";
 import image5 from "../assets/image5.png";
 import image6 from "../assets/image6.jpeg";
 import image7 from "../assets/image7.jpeg";
-import { userDataContext } from "../context/userContext";
+import { userDataContext } from "../context/UserContext";
 import { useNavigate } from "react-router-dom";
 import { MdKeyboardBackspace } from "react-icons/md";
 const Customize = () => {
